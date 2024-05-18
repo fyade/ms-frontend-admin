e18c98d50355a3e8d9647b6d9e749d6882ab28f0
FeiYang <fyghilt@qq.com>
2024-05-18 22:02:53 +0800

    0.0.03.10022 代码生成不想做了，以后再说

---
+++ src/views/sysUtil/formGeneration/index.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+export default {
+  name: 'sysUtil:formGeneration'
+}
+</script>
+<script setup lang="ts">
+
+</script>
+
+<template>
+表单生成
+</template>
+
+<style scoped>
+
+</style>
